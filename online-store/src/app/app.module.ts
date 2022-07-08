e28b1357976cd7816ad9967842e744751f888a8a
Mihail Dorozhko <dominick.alone@gmail.com>
2022-07-08 12:10:22 +0300

    feat: add empty components

--- online-store/src/app/app.module.ts
+++ online-store/src/app/app.module.ts
@@ -3,11 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CartComponent } from './components/cart/cart.component';
+import { SearchComponent } from './components/search/search.component';
+import { MainComponent } from './components/main/main.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { SortComponent } from './components/sort/sort.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductComponent } from './components/product/product.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CartComponent,
+    SearchComponent,
+    MainComponent,
+    FiltersComponent,
+    SortComponent,
+    ProductsComponent,
+    ProductComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
